ce84611eaeb590b50c2edd6e39a711226af4944a
Pavan <pavan.more1@gmail.com>
2025-08-07 20:42:37 +0530

    Initial Commit

---
+++ RandomList/RandomListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomListApp.swift
+//  RandomList
+//
+//  Created by Pavan More on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
